613e5b9cbfbdbbb1e07f51aaf3af754ebde0a3e6
Kithma A.P.O <it20228026@my.sliit.lk>
2023-09-16 14:01:16 +0530

    Include group member details

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,15 @@
 import Header from "../components/header";
 import CtaSection from "../components/cta-section";
 import Footer from "../components/footer";
+import OurTeam from "../components/our-team";
 
 export default function Home() {
     return (
         <div className="animated-bg">
             <Header/>
-            <main className="px-6 sm:px-8 md:px-12">
+            <main className="px-6 sm:px-8 md:px-12 py-5">
                 <CtaSection/>
+                <OurTeam/>
             </main>
             <Footer/>
         </div>
