8ece8f1a080e7737d4ebe0e838bd54272d70ee8b
Kithma A.P.O <it20228026@my.sliit.lk>
2023-09-30 15:41:08 +0530

    Initialize redux store

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,19 @@
 import Head from 'next/head'
-import { AppProps } from 'next/app'
+import {AppProps} from 'next/app'
 import '../styles/index.css'
+import {store} from '../store'
+import {Provider} from 'react-redux'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <title>NextJS TailwindCSS TypeScript Starter</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  )
+function MyApp({Component, pageProps}: AppProps) {
+    return (
+        <Provider store={store}>
+            <Head>
+                <title>NextJS TailwindCSS TypeScript Starter</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+            </Head>
+            <Component {...pageProps} />
+        </Provider>
+    )
 }
 
 export default MyApp
