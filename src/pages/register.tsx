f2ed04f18252fd3794d23808be6c1ac782f5b5dc
Flashminat0 <wyasaslowe@gmail.com>
2023-10-13 22:01:22 +0530

    swc error fix + image accept on register

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -145,7 +145,7 @@ const Register = () => {
                                     type="file"
                                     accept={'.jpg, .png, .jpeg'}
                                     onChange={handleImageFileChange}
-                                    className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                                      className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
                                 />
                             </div>
                         </div>
